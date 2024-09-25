6e8ddb472d8ac2eeae2ce3aa57dcb59ee9ca33ac
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-09-25 02:38:22 -0400

    fdf

--- src/pages/Home/Plans.tsx
+++ src/pages/Home/Plans.tsx
@@ -1,15 +1,49 @@
-import React from 'react'
-import { PlanCards } from './PlanCards'
-import {data} from '../../Data.ts'
+
 export const Plans = () => {
 
-  console.log(data)
   return (
-    <>
-    {data.map((item, index) => {
-      return <PlanCards key={index} {...item} />
-    })}
-    
-    </>
+   <div className="basic flex flex-col gap-5">
+     <div className="w-full flex flex-col gap-10 items-center">
+      <div className='flex flex-col gap-4'>
+        <h3 className="text-xl font-bold mb-4">Basic</h3>
+        <h1 className="text-6xl font bold">Free</h1>
+        <p>Up to 5 users for free</p>
+      </div>
+      <div className='flex flex-col gap-4'>
+        <p>Basic document collaboration</p>
+        <p>2 GB storage</p>
+        <p>Great security and support</p>
+      </div>
+      <button className="h-14 w-44 rounded-3xl">Get Started</button>
+    </div>
+
+     <div className="pro w-full flex flex-col gap-10 items-center">
+      <div className='flex flex-col gap-4'>
+        <h3 className="text-xl font-bold mb-4">Pro</h3>
+        <h1 className="text-6xl font bold">$9.99</h1>
+        <p > Per user, billed monthly</p>
+      </div>
+      <div className=' flex flex-col gap-4'>
+        <p>All essential inergrations</p>
+        <p>50 GB storage</p>
+        <p>More control and insights</p>
+      </div>
+      <button className="h-14 w-44 rounded-3xl">Try for Free</button>
+    </div>
+
+     <div className="ultimate w-full flex flex-col gap-10 items-center">
+      <div className='flex flex-col gap-4'>
+        <h3 className="text-xl font-bold mb-4">Ultimate</h3>
+        <h1 className="text-6xl font bold">$19.99</h1>
+        <p >Per user, billed monthly</p>
+      </div>
+      <div className='flex flex-col gap-4'>
+        <p>Robust work management</p>
+        <p>100 Gb storage</p>
+        <p>VIP support</p>
+      </div>
+      <button className="h-14 w-44 rounded-3xl">Try for free</button>
+    </div>
+   </div>
   )
 }
