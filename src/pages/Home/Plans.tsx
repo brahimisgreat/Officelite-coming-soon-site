add9b5655b027c4acc90507faec2d8d434981e01
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-09-25 03:14:04 -0400

    fdf

--- src/pages/Home/Plans.tsx
+++ src/pages/Home/Plans.tsx
@@ -1,12 +1,13 @@
+ import './Plans.scss'
 
 export const Plans = () => {
 
   return (
-   <div className="basic flex flex-col gap-5">
-     <div className="w-full flex flex-col gap-10 items-center">
+   <div className="plans w-full flex flex-col  gap-5">
+     <div className="basic w-full flex flex-col gap-10 items-center justify-center ">
       <div className='flex flex-col gap-4'>
         <h3 className="text-xl font-bold mb-4">Basic</h3>
-        <h1 className="text-6xl font bold">Free</h1>
+        <h1 className="text-6xl font-bold">Free</h1>
         <p>Up to 5 users for free</p>
       </div>
       <div className='flex flex-col gap-4'>
@@ -14,13 +15,13 @@ export const Plans = () => {
         <p>2 GB storage</p>
         <p>Great security and support</p>
       </div>
-      <button className="h-14 w-44 rounded-3xl">Get Started</button>
+      <button className="h-14 w-44 rounded-3xl "><span className='font-bold'>Try for free</span></button>
     </div>
 
-     <div className="pro w-full flex flex-col gap-10 items-center">
+     <div className="pro w-full flex flex-col gap-10 items-center justify-center">
       <div className='flex flex-col gap-4'>
         <h3 className="text-xl font-bold mb-4">Pro</h3>
-        <h1 className="text-6xl font bold">$9.99</h1>
+        <h1 className="text-6xl font-bold">$9.99</h1>
         <p > Per user, billed monthly</p>
       </div>
       <div className=' flex flex-col gap-4'>
@@ -31,10 +32,10 @@ export const Plans = () => {
       <button className="h-14 w-44 rounded-3xl">Try for Free</button>
     </div>
 
-     <div className="ultimate w-full flex flex-col gap-10 items-center">
+     <div className="ultimate w-full flex flex-col gap-10 items-center justify-center">
       <div className='flex flex-col gap-4'>
         <h3 className="text-xl font-bold mb-4">Ultimate</h3>
-        <h1 className="text-6xl font bold">$19.99</h1>
+        <h1 className="text-6xl font-bold">$19.99</h1>
         <p >Per user, billed monthly</p>
       </div>
       <div className='flex flex-col gap-4'>
