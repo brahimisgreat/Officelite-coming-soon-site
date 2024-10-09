7c4b69ce3d0568e40e3c088d878465d69589fe63
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-10-08 23:30:40 -0400

    fdf

--- src/pages/Home/Last.tsx
+++ src/pages/Home/Last.tsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export const Last = () => {
   return (
-    <div>Last</div>
+    <footer>
+        <div>
+            <h4>COMING<span>4 NOV 2020</span></h4>
+        </div>
+        <div className='flex'>
+            <h3 className='flex flex-col'>47 <span>days</span></h3>
+            <h3 className='flex flex-col'>07 <span>hours</span></h3>
+            <h3 className='flex flex-col'>57 <span>min</span></h3>
+            <h3 className='flex flex-col'>17 <span>sec</span></h3>
+        </div>
+        <div>
+            <button>Get Started</button>
+        </div>
+    </footer>
   )
 }
